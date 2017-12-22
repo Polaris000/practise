7f09b034428ba1e4e823bc2cff0ffd753beaffa9
Aniruddha Karajgi <akarajgi0@gmail.com>
2017-12-22 20:59:18 +0530

    Create project_euler_17.py

---
+++ project_euler_17.py
@@ -0,0 +1,4 @@
+# Print out the total number of letters used if all the numbers between 1 and 1000 (inclusive) were written as words:
+# Eg. 5 --> five = 4 letters.
+# Do not count spaces and hyphens, but count "and"
+
