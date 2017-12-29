3f87ef703e2b793a6eeabd30aaa4b99105005935
Aniruddha Karajgi <akarajgi0@gmail.com>
2017-12-29 18:51:29 +0530

    Update and rename fabonacci_using_recursion.md to fabonacci_using_recursion.py

---
+++ fabonacci_using_recursion.py
@@ -0,0 +1,17 @@
+
+#This code gives you fabonacci numbers less 100(you can change the value for that matter). Includes recursion
+def fib(a,b):
+  c=a+b
+  if c<=100:
+  
+    print(c)
+    a=b
+    b=c
+    fab(a,b)
+    
+a=0
+b=1
+
+print(a)
+print(b)
+fab(a,b)
