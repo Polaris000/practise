14d421689aa2958ca1178aee2ec354956ece5118
Aniruddha Karajgi <akarajgi0@gmail.com>
2018-07-19 16:00:58 +0530

    Update args_kwargs.py

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,8 +1,19 @@
-
+"""
+The concept of args and kwargs comes in handy when one is not aware of the number of arguments a method will be given
+args stands for arguments, akin to a python list
+kwargs stands for keyword arguments, akin to a python dictionary
+"""
 
 def foo(*args, **kwargs):
     print(args)
     print(kwargs)
 
-
+    
 foo("hello", "bye", 1, 2, gugu="hi")
+
+"""
+output:
+    ('hello', 'bye', 1, 2)
+    {'gugu': 'hi'}
+
+"""
