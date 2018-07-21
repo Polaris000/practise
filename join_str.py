c96584f71dcc565acc12e2c550e29a46ddaff248
Aniruddha Karajgi <akarajgi0@gmail.com>
2018-07-21 15:17:29 +0530

    Update join_str.py

--- join_str.py
+++ join_str.py
@@ -1,3 +1,12 @@
+"""
+This program uses the join() method for lists. The join method takes a list as an argument
+and joins the elements with the string on which the join method is called
+
+eg. c = " ".join(b)
+  output:
+    h e l l o  w o r l d
+"""
+
 a = "hello world"
 
 b = list(a)
