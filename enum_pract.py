cc9a74722a0d9a398b3c8ea12f99133c6401c6eb
Aniruddha Karajgi <akarajgi0@gmail.com>
2019-02-10 19:32:29 +0530

    Update enum_pract.py

--- enum_pract.py
+++ enum_pract.py
@@ -1,3 +1,8 @@
+# Regular iterations using a for loop allow us to access each element in a list, but not its index directly
+# To find, for example, multiple instances of the same element in the list, one can use enumerate()
+# This internally iterates over two lists --- the actual one and one storing the indices
+
+
 fruits = ["apple", "banana"]
 
 for index, fruit in enumerate(fruits, start=1):
